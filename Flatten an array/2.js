0874dd0cc010dc739d03259783930793fc047b48
Anh Le <kunquan2345@gmail.com>
2018-12-31 01:02:57 -0800

    Add comment for Flatten an array 2.js

--- Flatten an array/2.js
+++ Flatten an array/2.js
@@ -1,4 +1,5 @@
-// Fast and effective way to flatten an array
+// Shorter and more effective way to flatten an array
+// Note the function of concat() in combination with spread operator
 function flattenArray(arr) {
     let flat = [].concat(...arr);
     return flat.some(Array.isArray) ? flattenArray(flat) : flat;
